8f221ec9232800d00974075c83992b75579cec1a
J-Pinos3 <85661206+J-Pinos3@users.noreply.github.com>
2022-08-20 18:01:54 -0500

    Creación de los prototipos de las funciones para los médicos

--- Proyectos/include/medicos.h
+++ Proyectos/include/medicos.h
@@ -14,3 +14,14 @@ typedef struct{
     tDoctores elementos[MAX_DOCS];
     int contador = 0;
 }tLista;
+
+//PROTOTIPOS DE LAS FUNCIONES PARA LOS MÉDICOS
+int Menu();
+void Cargar(tLista &lista, bool& ok);
+void Guardar(const tLista &lista)
+void LeerDoctor(tDoctores &doctores);
+void InsertarDoctor(tLista &lista, tDoctores doctor, bool &ok);
+void EliminarDoctor(tLista &lista, int pos, bool &ok);
+
+void MostrarDoctor(tDoctores doctor);
+void ListadoDoctores(const tLista &lista);
