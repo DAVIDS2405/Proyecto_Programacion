2fd09edc7b6de8a543fc8da164e6009f5a692d9f
MiguelCarapaz <85420759+MiguelCarapaz@users.noreply.github.com>
2022-08-20 17:42:10 -0500

    Update citas.h
    citas

---
+++ Proyectos/include/citas.h
@@ -0,0 +1,16 @@
+#include <string>
+using namespace std;
+const int MAX_DOCS = 30;
+
+typedef struct{
+    string nombreMED;
+    string apellidoMED;
+    string edadMED;
+    string especialidad;
+    double sueldo;
+}tDoctores;
+
+typedef struct{
+    tDoctores elementos[MAX_DOCS];
+    int contador = 0;
+}tLista;
