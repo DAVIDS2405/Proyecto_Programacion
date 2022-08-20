3cb9265d87324eaf6177d8e3c528a8ec7b34845f
MiguelCarapaz <85420759+MiguelCarapaz@users.noreply.github.com>
2022-08-20 17:54:39 -0500

    Update pacientes.h

--- Proyectos/include/pacientes.h
+++ Proyectos/include/pacientes.h
@@ -1,3 +1,16 @@
-typedef struct {
+#include <string>
+using namespace std;
+const int MAX_PACIE = 30;
 
+typedef struct{
+    string nombrePAC;
+    string apellidoPAC;
+    string edadPAC;
+    string cedulaPAC;
 }tPacientes;
+
+typedef struct{
+    tPacientes elementos[MAX_PACIE];
+    int contador = 0;
+}tLista;
+
