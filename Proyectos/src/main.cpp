6bf2d9243e2607c45a0f769498d51629621bae43
David Basantes <sebastian2405lucero@hotmail.com>
2022-08-23 18:26:40 -0500

    cambio main

--- Proyectos/src/main.cpp
+++ Proyectos/src/main.cpp
@@ -93,16 +93,18 @@ int main(){
     return 0;
 }
 
-int MenuPrincipal(){
+int MenuPrincipal()
+{
     int op = 0;
     while ((op < 1) || (op > 4))
     {
         cout << setw(41) << " --------------------------------------------" << endl;
-        cout << "|" << setw(30) << "Menu Pacientes" << setw(15) << "|" << endl;
-        cout << "|" << setw(24) << "1 - Ingresar a Medicos" << setw(21) << "|" << endl;
-        cout << "|" << setw(25) << "2 - Ingresar a Pacientes" << setw(20) << "|" << endl;
-        cout << "|" << setw(25) << "3 - Ingresar a Citas" << setw(20) << "|" << endl;
+        cout << "|" << setw(30) << "CITAS MEDICAS" << setw(15) << "|" << endl;
+        cout << "|" << setw(36) << "1 - Ingresar a Medicos" << setw(9) << "|" << endl;
+        cout << "|" << setw(38) << "2 - Ingresar a Pacientes" << setw(7) << "|" << endl;
+        cout << "|" << setw(34) << "3 - Ingresar a Citas" << setw(11) << "|" << endl;
         cout << "|" << setw(23) << "4 - Salir" << setw(22) << "|" << endl;
+        cout << setw(41) << " --------------------------------------------" << endl;
         cout << setw(10) << " Ingrese la opcion deseada: ";
         cin >> op;
         cin.ignore();
