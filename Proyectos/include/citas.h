9b9208d9917490485b7f2b2692873884daaeec4b
J-Pinos3 <85661206+J-Pinos3@users.noreply.github.com>
2022-08-30 14:47:06 -0500

    cambios en cita y paciente

--- Proyectos/include/citas.h
+++ Proyectos/include/citas.h
@@ -1,34 +1,36 @@
+#include <string>
+using namespace std;
 const int MAX_DOCS_AUX = 30;
 const int MAX_PACIE_AUX = 30;
 const int MAX_CITAS = 30;
 
 //ESTRUCTURAS PARA DOCTORES
 typedef struct{
-    string nombreMED;
-    string apellidoMED;
-    int edadMED;
-    string especialidad;
-    string cedula;
+    string nombre_MED;
+    string apellido_MED;
+    int edad_MED;
+    string especialidad_MED;
+    string cedula_MED;
 }tDoctoresAUX;
 
 typedef struct{
-    tDoctoresAUX elementosDoc[MAX_DOCS_AUX];
-    int contadorDoc = 0;
+    tDoctoresAUX Doc_aux[MAX_DOCS_AUX];
+    int contadorDoc_aux = 0;
 }tListaDocAUX;
 
 //ESTRUCTURAS PARA PACIENTES
 typedef struct
 {
-    string nombrePAC;
-    string apellidoPAC;
-    string edadPAC;
-    string cedulaPAC;
+    string nombre_PAC;
+    string apellido_PAC;
+    string edad_PAC;
+    string cedula_PAC;
 } tPacientesaux;
 
 typedef struct
 {
-    tPacientesaux elementos[MAX_PACIE_AUX];
-    int contador = 0;
+    tPacientesaux Pac_aux[MAX_PACIE_AUX];
+    int contadorPac_aux = 0;
 } tListaPacienteAUX;
 
 
@@ -36,8 +38,10 @@ typedef struct
 typedef struct{
       int codigo_cita;
       int dia, mes, anio;
-      tDoctoresAUX elementosDoc[MAX_DOCS_AUX];
-      tPacientesaux elementos[MAX_PACIE_AUX];
+      string cedDoc,nomDoc, apelDoc, especialidadDoc;
+      string cedPax,nomPax, apelPax;
+      //tDoctoresAUX elementosDoc[MAX_DOCS_AUX];
+      //tPacientesaux elementos[MAX_PACIE_AUX];
       //tDoctoresAUX doctor;
       //tPacientesaux paciente;
 }tCita;
@@ -53,7 +57,7 @@ void Cargar_docs_aux(tListaDocAUX &listaAux, bool& funca);
 void Cargar_pacs_aux(tListaPacienteAUX &listaAux, bool& funca);
 void Cargar_citas(tListaC &listacita, bool& funca);
 void Guardar_citas(const tListaC &listacita);
-void Leer_citas(tCita &cita, tListaDocAUX &listaAux, tListaPacienteAUX &lista);
+void Leer_citas(tCita &cita, tListaDocAUX &listaAux, tListaPacienteAUX &listaAux);
 void Insertar_cita(tListaC &listacita, tCita cita, bool &funca);
 void Eliminar_cita(tListaC &listacita, int pos, bool &funca);
 void Ordenar_cita(tListaC &listacita);
--- Proyectos/include/pacientes.h
+++ Proyectos/include/pacientes.h
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string>
 using namespace std;
 const int MAX_PACIE = 30;
